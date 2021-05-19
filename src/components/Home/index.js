d30700cf09572f9979cf48a356d6770b6198cf2c
Ramon Celestino <ramoncelesteramos@gmail.com>
2021-05-19 16:38:59 -0300

    Home adjusts

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,43 +4,43 @@ import styles from './home.module.scss';
 const Home = () => {
   return (
     <div className={styles.home}>
-      <div className="row">
-        <div className="col-sm-6 col-md-4">
+      <div className={styles.row}>
+        <div className={styles.column}>
           <CardModule
             name="Clientes"
             icon="fas fa-users fa-3x"
             link="/customers/"
           />
         </div>
-        <div className="col-sm-6 col-md-4">
+        <div className={styles.column}>
           <CardModule
             name="Financeiro"
             icon="fas fa-dollar-sign fa-3x"
             link="/clientes"
           />
         </div>
-        <div className="col-sm-6 col-md-4">
+        <div className={styles.column}>
           <CardModule
             name="Produtos"
             icon="fas fa-birthday-cake fa-3x"
             link="/clientes/"
           />
         </div>
-        <div className="col-sm-6 col-md-4">
+        <div className={styles.column}>
           <CardModule
             name="Encomendas"
             icon="fas fa-shopping-cart fa-3x"
             link="/orders/"
           />
         </div>
-        <div className="col-sm-6 col-md-4">
+        <div className={styles.column}>
           <CardModule
             name="Estoque"
             icon="fas fa-clipboard fa-3x"
             link="/clientes"
           />
         </div>
-        <div className="col-sm-6 col-md-4">
+        <div className={styles.column}>
           <CardModule
             name="Outros"
             icon="fas fa-trash fa-3x"
